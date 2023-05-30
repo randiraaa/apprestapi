2fd0952f4f897e682a532af00fc1c0efe7340cab
Randira <204060028.mhs@stmikjabar.ac.id>
2023-05-30 10:22:40 +0700

    add server

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const port = 3000;
+
+// Parse application/json
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}...`);
+});
