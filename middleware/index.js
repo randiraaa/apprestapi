1190b84f294842666d5e431fd817282b0fcf6c91
Randira <204060028.mhs@stmikjabar.ac.id>
2023-05-31 15:27:36 +0700

    add link bridge authorization

--- middleware/index.js
+++ middleware/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const auth = require("./auth");
+const verifikasi = require("./verifikasi");
 const router = express.Router();
 
 // Mendaftarkan url registrasi
@@ -7,5 +8,8 @@ router.post("/api/v1/register", auth.registrasi);
 
 // Mendaftarkan url login
 router.post("/api/v1/login", auth.login);
+
+// Alamat yang perlu otorisasi
+router.get("/api/v1/rahasia", verifikasi(2), auth.halamanRahasia);
 
 module.exports = router;
