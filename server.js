41de0c02334134d51e1314add2b1a2e9431efa8f
Randira <204060028.mhs@stmikjabar.ac.id>
2023-05-31 12:06:08 +0700

    add routes auth

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const morgan = require("morgan");
 const app = express();
 const port = 3000;
 
 // Parse application/json
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(morgan("dev"));
 
 // Panggil routes.js
 const routes = require("./routes");
 routes(app);
+
+// Daftarkan menu routes dari index
+app.use("/auth", require("./middleware"));
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}...`);
